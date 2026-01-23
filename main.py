b1c0ae6fdf98e670e0fdded417283f6fd8c3aee4
deborq67 <debohorquez@mail.fhsu.edu>
2026-01-23 00:48:10 -0600

    Added stocks related to travel.

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # INF601 - Advanced Programming in Python
 # David Bohorquez
 # Mini Project 1
+
+#Import the required modules:
+
+import yfinance as yf
+
+
+Marriott = yf.Ticker("MAR")
+Hilton = yf.Ticker("HLT")
+RoyalCaribbean = yf.Ticker("RCL")
+Southwest = yf.Ticker("LUV")
+Uber = yf.Ticker("UBER")
+
+# Get Marriott historical market data
+# Marriott.history(period='1')
